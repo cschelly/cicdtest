e5744567fd47fa5a87421245cf88a0628b2df4c3
Christina <christina.sund@tealium.com>
2020-07-20 10:08:14 -0700

    initial

---
+++ CICDTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CICDTest
+//
+//  Created by Christina S on 7/20/20.
+//  Copyright © 2020 Tealium. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
